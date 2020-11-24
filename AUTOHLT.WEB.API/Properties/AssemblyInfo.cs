64169a2f87e93b7e06450d9057fa9c853f7d4733
BongLV <bonglv@outlook.com>
2020-11-24 05:05:49 +0000

    Merged PR 165: update lại cấu trúc project

--- AUTOHLT.WEB.API/Properties/AssemblyInfo.cs
+++ AUTOHLT.WEB.API/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -20,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f0d14dcb-3c7d-435d-8b46-9773d3fc3362")]
+[assembly: Guid("421feee3-aebf-4484-9d2c-abc5f9c0b47f")]
 
 // Version information for an assembly consists of the following four values:
 //
