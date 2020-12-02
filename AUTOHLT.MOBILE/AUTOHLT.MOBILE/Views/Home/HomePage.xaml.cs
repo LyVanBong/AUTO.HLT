0da01d4b8b634a5ae629e32dabad8d7b67e8d05e
Bong Ly Van <BongLV@fpts.com.vn>
2020-12-02 13:16:42 +0700

    Cập nhật màu backgroup and màu chứ

--- AUTOHLT.MOBILE/AUTOHLT.MOBILE/Views/Home/HomePage.xaml.cs
+++ AUTOHLT.MOBILE/AUTOHLT.MOBILE/Views/Home/HomePage.xaml.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 using Xamarin.Forms.Xaml;
 
 namespace AUTOHLT.MOBILE.Views.Home
@@ -15,6 +12,15 @@ namespace AUTOHLT.MOBILE.Views.Home
         public HomePage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            On<iOS>().SetUseSafeArea(true);
+            var safeInsets = On<iOS>().SafeAreaInsets();
+            safeInsets.Bottom = -20;
+            Padding = safeInsets;
         }
 
         protected override bool OnBackButtonPressed()
