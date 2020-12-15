4890c8432db769fb04ca2b0055a93bd48bceecfb
BongLV <hopehappyday@outlook.com>
2020-12-15 21:14:41 +0700

    Thêm chức năng lây cookie

---
+++ AUTOHLT.MOBILE/AUTOHLT.MOBILE.iOS/CustomRenderer/WebViewiOsRenderer.cs
@@ -0,0 +1,32 @@
+﻿using AUTOHLT.MOBILE.iOS.CustomRenderer;
+using WebKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+
+[assembly: ExportRenderer(typeof(AUTOHLT.MOBILE.CustomRenderer.WebViewRenderer), typeof(WebViewiOsRenderer))]
+namespace AUTOHLT.MOBILE.iOS.CustomRenderer
+{
+    public class WebViewiOsRenderer : WkWebViewRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                NavigationDelegate = new CookieNavigationDelegate();
+            }
+        }
+    }
+
+    public class CookieNavigationDelegate : WKNavigationDelegate
+    {
+        public override void DidFinishNavigation(WKWebView webView, WKNavigation navigation)
+        {
+            webView.Configuration.WebsiteDataStore.HttpCookieStore.GetAllCookies((cookies) =>
+            {
+                //Lấy cookie tại đây
+            });
+        }
+    }
+}
