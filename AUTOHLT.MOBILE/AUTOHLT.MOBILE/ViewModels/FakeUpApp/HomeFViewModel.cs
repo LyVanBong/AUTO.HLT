d8fa54b3f33759a402b9c4bca58d6ff680dbdd04
BongLV <hopehappyday@outlook.com>
2021-01-09 18:39:31 +0700

    Cập nhật code ios

--- AUTOHLT.MOBILE/AUTOHLT.MOBILE/ViewModels/FakeUpApp/HomeFViewModel.cs
+++ AUTOHLT.MOBILE/AUTOHLT.MOBILE/ViewModels/FakeUpApp/HomeFViewModel.cs
@@ -15,13 +15,44 @@ namespace AUTOHLT.MOBILE.ViewModels.FakeUpApp
         {
             _pageDialogService = pageDialogService;
             LogoutCommand = new Command(Logout);
-            NavigationCommand = new Command(NavigationApp);
+            NavigationCommand = new Command<string>(NavigationApp);
         }
 
-        private async void NavigationApp()
+        private async void NavigationApp(string key)
         {
-            await _pageDialogService.DisplayAlertAsync("Thông báo",
-                $"Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi\nCông việc đã hoàn thành.\n{DateTime.Now.ToString("F")}", "OK");
+            await _pageDialogService.DisplayAlertAsync("Buff like",
+                $"bạn đã sử dụng tính năng buff like thành công", "OK");
+            switch (key)
+            {
+                case "0":
+                    await _pageDialogService.DisplayAlertAsync("Buff view",
+                        $"Đã buff view thành công", "OK");
+                    break;
+                case "1":
+                    await _pageDialogService.DisplayAlertAsync("Buff share",
+                        $"Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi\nCông việc đã hoàn thành.\n{DateTime.Now.ToString("F")}", "OK");
+                    break;
+                case "2":
+                    await _pageDialogService.DisplayAlertAsync("Buff comment",
+                        $"Đã comment tất cả các bài viết xong", "OK");
+                    break;
+                case "3":
+                    await _pageDialogService.DisplayAlertAsync("Buff follower",
+                        $"Đã tăng lượng follow theo yêu cầu của bạn thành công", "OK");
+                    break;
+                case "4":
+                    await _pageDialogService.DisplayAlertAsync("tuong tac",
+                        $"Đã tương tác với tất cả bạn bè xong", "OK");
+                    break;
+                case "5":
+                    await _pageDialogService.DisplayAlertAsync("Thông báo",
+                        $"Nhiệm vụ mới đã được thêm vào nhiệm vụ của bạn", "OK");
+                    break;
+                case "6":
+                    await _pageDialogService.DisplayAlertAsync("Thông báo",
+                        $"Thời gian hiện tại là: {DateTime.Now.ToString("F")}", "OK");
+                    break;
+            }
         }
 
         private async void Logout()
