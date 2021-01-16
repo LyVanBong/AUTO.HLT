3329c4bf78b028a5e0190806fcb895ffeea5aa51
BongLV <hopehappyday@outlook.com>
2021-01-17 06:40:22 +0700

    Cập nhật theme

---
+++ AUTOHLT.MOBILE/AUTOHLT.MOBILE/ThemesApp/LightTheme.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace AUTOHLT.MOBILE.ThemesApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LightTheme
+    {
+        public LightTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
