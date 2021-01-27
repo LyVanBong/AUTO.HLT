375c96ebf282f09da12a093a0d2dd9f4dbde0ad5
BongLV <hopehappyday@outlook.com>
2021-01-27 22:53:29 +0700

    Thêm thông báo trên màn hình home

---
+++ AUTO.HLT.CRM/Models/BalanceTwilio/BalanceTwilioModel.cs
@@ -0,0 +1,16 @@
+﻿namespace AUTO.HLT.CRM.Models.BalanceTwilio
+{
+    public class BalanceTwilioModel
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+        public Data Data { get; set; }
+    }
+
+    public class Data
+    {
+        public string currency { get; set; }
+        public string balance { get; set; }
+        public string account_sid { get; set; }
+    }
+}
