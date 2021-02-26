fd9c6839f683e670297d2a4c08dc5c51e45d098f
Bong Ly Van <BongLV@fpts.com.vn>
2021-02-26 18:20:16 +0700

    Tạo giao diện login

--- AUTO.HLT.MOBILE.VIP/AUTO.HLT.MOBILE.VIP/ViewModels/Login/LoginViewModel.cs
+++ AUTO.HLT.MOBILE.VIP/AUTO.HLT.MOBILE.VIP/ViewModels/Login/LoginViewModel.cs
@@ -1,7 +1,22 @@
-﻿namespace AUTO.HLT.MOBILE.VIP.ViewModels.Login
+﻿using AUTO.HLT.MOBILE.VIP.Views.Login;
+using Prism.Navigation;
+using Xamarin.Forms;
+
+namespace AUTO.HLT.MOBILE.VIP.ViewModels.Login
 {
-    public class LoginViewModel
+    public class LoginViewModel : ViewModelBase
     {
-        
+        private View _contentLoginPage;
+
+        public View ContentLoginPage
+        {
+            get => _contentLoginPage;
+            set => SetProperty(ref _contentLoginPage, value);
+        }
+
+        public LoginViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            ContentLoginPage = new LoginView();
+        }
     }
 }
--- AUTO.HLT.MOBILE.VIP/AUTO.HLT.MOBILE.VIP/Views/Login/LoginPage.xaml.cs
+++ AUTO.HLT.MOBILE.VIP/AUTO.HLT.MOBILE.VIP/Views/Login/LoginPage.xaml.cs
@@ -1,20 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace AUTO.HLT.MOBILE.VIP.Views.Login
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
         public LoginPage()
         {
             InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            On<iOS>().SetUseSafeArea(true);
+            var safeInsets = On<iOS>().SafeAreaInsets();
+            safeInsets.Bottom = -20;
+            Padding = safeInsets;
         }
     }
 }
