0735b1d3bcee63a9e66e8aafc710a7fdcf90b78d
Bong Ly Van <BongLV@fpts.com.vn>
2021-03-09 18:03:51 +0700

    update code

--- AUTO.HLT.MOBILE.VIP/AUTO.HLT.MOBILE.VIP/Views/HappyBirthday/HappyBirthdayPage.xaml.cs
+++ AUTO.HLT.MOBILE.VIP/AUTO.HLT.MOBILE.VIP/Views/HappyBirthday/HappyBirthdayPage.xaml.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 using Xamarin.Forms.Xaml;
 
 namespace AUTO.HLT.MOBILE.VIP.Views.HappyBirthday
@@ -15,6 +12,14 @@ namespace AUTO.HLT.MOBILE.VIP.Views.HappyBirthday
         public HappyBirthdayPage()
         {
             InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            On<iOS>().SetUseSafeArea(true);
+            var safeInsets = On<iOS>().SafeAreaInsets();
+            safeInsets.Bottom = -20;
+            Padding = safeInsets;
         }
     }
 }
