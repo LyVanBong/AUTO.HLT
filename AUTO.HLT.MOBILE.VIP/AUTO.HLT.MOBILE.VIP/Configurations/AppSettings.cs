a8163c09fa44a22f35e548d1ee56fcccdeb57283
BongLV <bonglv@outlook.com>
2021-03-11 08:08:11 +0000

    Merged PR 30: sửa lỗi tồn đọng
    Related work items: #17

--- AUTO.HLT.MOBILE.VIP/AUTO.HLT.MOBILE.VIP/Configurations/AppSettings.cs
+++ AUTO.HLT.MOBILE.VIP/AUTO.HLT.MOBILE.VIP/Configurations/AppSettings.cs
@@ -1,7 +1,10 @@
 ﻿namespace AUTO.HLT.MOBILE.VIP.Configurations
 {
-    public class AppSettings
+    public static class AppSettings
     {
-        
+        /// <summary>
+        /// khóa bản quyền của thư viện Syncfusion 
+        /// </summary>
+        public const string LICENSE_KEY_SYNCFUSION = "NDEwNTYwQDMxMzgyZTM0MmUzMFNFQjIrWGM3SjhUOWVLckZqTDIyOGY2b3puQmZWVzRydlhFY3d1SUNHZ289";
     }
 }
