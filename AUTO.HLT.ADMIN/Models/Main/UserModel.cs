9b72bd618619eec238bbdb386c432807d6bc8cca
Bong Ly Van <bonglv@outlook.com>
2021-03-15 23:55:21 +0700

    capaj nhat code

---
+++ AUTO.HLT.ADMIN/Models/Main/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AUTO.HLT.ADMIN.Models.Main
+{
+    public class UserModel
+    {
+        public int STT { get; set; }
+        public string ID { get; set; }
+        public DateTime NgayDangKy { get; set; }
+        public int ThoiHan { get; set; }
+        public string Cookie { get; set; }
+        public string Token { get; set; }
+        public bool ThaTim { get; set; }
+        public bool TuongTacAnhDaiDien { get; set; }
+        public int TrangThai { get; set; }
+        public string Note { get; set; }
+        public DateTime NgayHetHan => NgayDangKy.AddDays(ThoiHan);
+    }
+}
--- AUTO.HLT.ADMIN/ViewModels/Main/MainWindowViewModel.cs
+++ AUTO.HLT.ADMIN/ViewModels/Main/MainWindowViewModel.cs
@@ -1,10 +1,18 @@
-﻿using Prism.Mvvm;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using AUTO.HLT.ADMIN.Models.Main;
+using Prism.Commands;
+using Prism.Mvvm;
 
 namespace AUTO.HLT.ADMIN.ViewModels.Main
 {
     public class MainWindowViewModel : BindableBase
     {
         private string _title = "Công cụ hỗ trợ facebook";
+        private List<UserModel> _dataUsers;
+        private int _stt;
+        private string _id;
 
         public string Title
         {
@@ -12,8 +20,41 @@ namespace AUTO.HLT.ADMIN.ViewModels.Main
             set { SetProperty(ref _title, value); }
         }
 
+        public List<UserModel> DataUsers
+        {
+            get => _dataUsers;
+            set => SetProperty(ref _dataUsers, value);
+        }
+
+        public ICommand SaveAccountCommand { get; private set; }
+        public ICommand DeleteAccountCommand { get; private set; }
+
+        public int STT
+        {
+            get => _stt;
+            set => SetProperty(ref _stt,value);
+        }
+
+        public string ID
+        {
+            get => _id;
+            set => SetProperty(ref _id, value);
+        }
+
         public MainWindowViewModel()
         {
+            SaveAccountCommand = new DelegateCommand(SaveAccount);
+            DeleteAccountCommand = new DelegateCommand(DeleteAccount);
+        }
+
+        private void DeleteAccount()
+        {
+            MessageBox.Show("test 1");
+        }
+
+        private void SaveAccount()
+        {
+            MessageBox.Show("test 2");
         }
     }
 }
