654f425e790b9fa5d47b580002cb2c8e3e227be9
Bong Ly Van <BongLV@fpts.com.vn>
2021-03-30 20:10:25 +0700

    Thêm màn hình fake app

---
+++ AUTOHLT.MOBILE/AUTOHLT.MOBILE/Views/FakeUpApp/Views/HomePage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace AUTOHLT.MOBILE.Views.FakeUpApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : Xamarin.Forms.ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AUTOHLT.MOBILE/AUTOHLT.MOBILE/Views/FakeUpApp/Views/TabbedView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AUTOHLT.MOBILE.Views.FakeUpApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedView : TabbedPage
+    {
+        public TabbedView()
+        {
+            InitializeComponent();
+        }
+    }
+}
