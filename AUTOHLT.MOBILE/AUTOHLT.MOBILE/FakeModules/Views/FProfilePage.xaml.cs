733d6e6c9b4cdaacc94b2ab8cdb206ec904f1578
Bong Ly Van <bonglv@outlook.com>
2021-05-26 13:54:33 +0700

    Cập nhật app fake

--- AUTOHLT.MOBILE/AUTOHLT.MOBILE/FakeModules/Views/FProfilePage.xaml.cs
+++ AUTOHLT.MOBILE/AUTOHLT.MOBILE/FakeModules/Views/FProfilePage.xaml.cs
@@ -15,8 +15,9 @@ namespace AUTOHLT.MOBILE.FakeModules.Views
         protected override void OnAppearing()
         {
             base.OnAppearing();
+            On<iOS>().SetUseSafeArea(true);
             var safeInsets = On<iOS>().SafeAreaInsets();
-            safeInsets.Left = 20;
+            safeInsets.Bottom = 0;
             Padding = safeInsets;
         }
     }
