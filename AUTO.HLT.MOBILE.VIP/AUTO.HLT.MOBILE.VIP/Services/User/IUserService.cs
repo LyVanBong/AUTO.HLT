509d7933079e91d702505bb064c49d4c1df68f87
Bong Ly Van <bonglv@outlook.com>
2021-06-07 02:50:11 +0700

    Thêm tính năng đổi thưởng trên autovip

--- AUTO.HLT.MOBILE.VIP/AUTO.HLT.MOBILE.VIP/Services/User/IUserService.cs
+++ AUTO.HLT.MOBILE.VIP/AUTO.HLT.MOBILE.VIP/Services/User/IUserService.cs
@@ -8,6 +8,19 @@ namespace AUTO.HLT.MOBILE.VIP.Services.User
     public interface IUserService
     {
         /// <summary>
+        /// Cộng xu cho người dùng
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="price"></param>
+        /// <returns></returns>
+        Task<int> SetPriceUser(string userName, string price);
+        /// <summary>
+        /// lấy số tiền hiên tại của user
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns></returns>
+        Task<int> GetPriceUser(string userName);
+        /// <summary>
         /// Lấy danh sách tất cả user
         /// </summary>
         /// <returns></returns>
